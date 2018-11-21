86f44326853d90bd419011c6d1fb9f30200f6ead
Jasmine Carbajal <jaz@Jasmines-MacBook-Pro.local>
2018-11-20 18:04:27 -0800

    update for day 4

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { TodoInterface } from '../interfaces/todo-interface';
 import { Todo } from '../models/todo';
 import { TODOS } from '../mocks/mock-todos';
 
@@ -10,19 +10,19 @@ import { TODOS } from '../mocks/mock-todos';
 })
 export class TodoComponent implements OnInit {
 
-  todos: Todo[] = TODOS;
-  newTodo: Todo = new Todo();
+  todos: TodoInterface[] = TODOS;
+  newTodo: TodoInterface = new Todo();
 
   constructor() { }
 
   addTodo() {
     const lastIndex: number = this.todos.length - 1;
-    const last: Todo = this.todos[lastIndex];
+    const last: TodoInterface = this.todos[lastIndex];
     this.newTodo.id =  last.id + 1;
-    this.newTodo.isCompleted = false;
-    this.newTodo.isDeleted = false;
-    this.todos.push(this.newTodo);
-    this.newTodo = new Todo();
+    if (this.newTodo.name.trim().length >= 1) {
+      this.todos.push(this.newTodo);
+      this.newTodo = new Todo();
+    }
   }
 
   toggleCompleteTodo(todo) {
@@ -33,8 +33,19 @@ export class TodoComponent implements OnInit {
     todo.isDeleted = !todo.isDeleted;
   }
 
-  renderIsCompletedText(todo) {
+  deleteTodo(todo) {
+    const remove: number = this.todos.indexOf(todo);
+    if (remove !== -1) {
+      this.todos.splice(remove, 1);
+    }
+  }
+
+  renderIsCompletedText(todo: TodoInterface) {
     return todo.isCompleted ? 'Uncomplete' : 'Complete';
+  }
+
+  renderText(tags): String {
+    return tags.join(', ');
   }
 
   ngOnInit() {
